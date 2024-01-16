dbd6a4753d799c8fc2fdf0747759c4ee0fc1b505
Bhoomika <bhoomikahm18@gmail.com>
2024-01-16 14:47:00 +0530

    Adding Header component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header/Header.jsx';
+
+function App() {
+  return (
+    <div >
+      <Header />
+      {
+        // Home Page
+      }
+    </div>
+  );
+}
+
+export default App;
