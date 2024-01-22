7ec2983a06969385783d2fc02d8cdfa99371d1a0
Bhoomika <bhoomikahm18@gmail.com>
2024-01-22 13:09:50 +0530

    Adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header.jsx';
+import HomePage from './components/HomePage/HomePage.jsx';
+import Movies from './components/Movies/Movies.jsx';
+import Admin from './components/Admin/Admin.jsx';
+import Auth from './components/Auth/Auth.jsx';
 
 function App() {
   return (
     <div >
       <Header />
-      {
-        // Home Page
-      }
+      <section>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/movies' element={<Movies />} />
+          <Route path='/admin' element={<Admin />} />
+          <Route path='/auth' element={<Auth />} />
+        </Routes>
+      </section>
     </div>
   );
 }
